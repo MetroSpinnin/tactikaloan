09c7feb67ca748670dc8b5a74a7257526f30967d
MetroSpinnin <jimsamuel50@gmail.com>
2019-05-25 04:14:17 -0700

    Refactored index designs via larafile

---
+++ resources/views/layouts/index.blade.php
@@ -0,0 +1,374 @@
+
+<!doctype html>
+<html lang="en">
+
+  <head>
+   <meta charset="utf-8">
+    <title>Tactika</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Tactika">
+    <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,400i,500" rel="stylesheet">
+    <link href="{{assets(css/socicon.css)}}" rel="stylesheet" type="text/css" media="all" />
+    <link href="{{assets(css/entypo.css)}}" rel="stylesheet" type="text/css" media="all" />
+    <link href="{{assets(css/theme.css)}}" rel="stylesheet" type="text/css" media="all" />
+    <style>@font-face{font-family:all; src:url(fonts/fonts/Nunito-Regular.woff)format('woff');} *{font-family: all;} .bg-darky{background:lavender !important;}.dotter{border:2px dashed green; border-radius:100%;}.bg-gradient {background: linear-gradient(135deg, #283c86 0%, #45a247 100%),url("assets/img/be22.png") !important;}.navabar-toggler{color:green}</style>
+  </head>
+
+  <body>
+   <div class="navbar-container">
+      <div class="bg-darky navbar-dark" data-sticky="top">
+        <div class="container">
+          <nav class="navbar navbar-expand-lg">
+            <a class="navbar-brand" href="index.php">
+              <img alt="Tactika" src="assets/img/logo.png" width="120px" />
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon bg-success" style="border: 1px dashed green !important; padding:5px !important;"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-between" id="navbarNav">
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <a href="loan.php" class="nav-link"><font color="green">Loan</font></a>
+                </li>
+                <li class="nav-item">
+                  <a href="investors.php" class="nav-link"><font color="green">Investors</font></a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link" href="#" id="pagesDropdown" role="button" data-toggle="dropdown"><font color="green">Contact Us</font></a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link" href="#faq" id="componentsDropdown" role="button" data-toggle="dropdown"><font color="green">FAQs</font></a>
+                  </li>
+              </ul>
+
+              <ul class="navbar-nav">
+                <li class="nav-item d-flex align-items-center">
+                  <a href="#" class="btn btn-success"><b>Login Now</b></a>
+                </li>
+              </ul>
+
+            </div>
+            <!--end nav collapse-->
+          </nav>
+        </div>
+        <!--end of container-->
+      </div>
+    </div>
+    <div class="main-container">
+      <section class="space-lg bg-gradient">
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-12 col-md-6 mb-5 mb-md-0 position-relative">
+              <h1 class="display-4">Get a loan from Tactika in 10 Minutes</h1>
+              <p align="justify">
+                Whether you are a student or Market woman or Business person or Salary earner Get a loan from Tactika in 10 Minutes and also technical support during transactions almost immediately as required.
+              </p>
+            </div>
+            <!--end of col-->
+            <div class="col-12 col-md-6">
+              <div class="overflow-visible" data-flickity='{ "cellAlign": "center", "contain": true, "prevNextButtons": false, "pageDots":false, "wrapAround":true, "autoPlay":5000, "imagesLoaded":true, "initialIndex":3, "draggable":false }'>
+
+                <div class="carousel-cell col-11" style="background:lavender; border-radius:5px; padding:20px;">
+                  <form>
+                  <div class="form-group">
+                  <input type="text" name="" placeholder ="User name" class="form-control"/>
+                  </div>
+                  <div class="form-group">
+                  <input type="text" name="" placeholder ="email" class="form-control"/>
+                  </div>
+                  <div class="form-group">
+                  <input type="text" name="" placeholder ="password" class="form-control"/>
+                  </div>
+                  <div class="form-group">
+                  <input type="text" name="" placeholder ="Confirm password" class="form-control"/>
+                  </div>
+                  <input type="submit" class="btn btn-success" value="Register"/>
+                  </form>
+                </div>
+              </div>
+            </div>
+            <!--end of col-->
+          </div>
+          <!--end of row-->
+        </div>
+        <!--end of container-->
+      </section>
+      <!--end of section-->
+     <section class="bg-white">
+        <div class="container">
+          <div class="row justify-content-around align-items-center">
+            <div class="col-12 col-md-6 col-lg-5 text-center text-md-left section-intro">
+              <span class="title-decorative">Loans Curated for all</span>
+              <h3 class="h1">Quick, Lowest Interests</h3>
+              <span class="lead">
+               Tactika employs industry-standard Low Interest Loan rates with an intuitive workflow designed to get you up and runnning faster.
+              </span>
+              <a href="loan.php">Apply for Loans &rsaquo;</a>
+            </div>
+            <!--end of col-->
+            <div class="col-8 col-md-6 col-lg-4">
+              <img alt="Image" src="assets/img/graphic-developer-group.svg" class="img-fluid shadow" />
+            </div>
+            <!--end of col-->
+          </div>
+          <!--end of row-->
+        </div>
+        <!--end of container-->
+      </section>
+      <!--end of section-->
+      <section class="bg-gradient text-white space-lg">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-12 col-lg-10">
+              <div class="media">
+                <img alt="Image" src="assets/img/avatar-male-3.jpg" class="avatar avatar-lg" />
+                <div class="media-body">
+                  <p class="h2">
+                    “With Tactika, you get your desired Loan amount with lowest interest rates in less than 10 Minutes.”
+                  </p>
+                  <span>Samuel Metro</span>
+                </div>
+              </div>
+            </div>
+            <!--end of col-->
+          </div>
+          <!--end of row-->
+        </div>
+        <!--end of container-->
+      </section>
+      <!--end of section-->
+      <section>
+        <div class="container">
+          <div class="row justify-content-center text-center section-intro">
+            <div class="col-12 col-md-9 col-lg-8">
+              <span class="title-decorative" data-aos="fade-up" data-aos-delay="10">Missions &amp; Visions</span>
+              <h2 class="display-4" data-aos="fade-up" data-aos-delay="100">About us + much more</h2>
+              <span class="lead" data-aos="fade-up" data-aos-delay="200">Tactika is a financial solution provider for delivering tailor made offerings for our many customers using technology as an enabler and push them to helping achieve their goals be it personal family or business.</span>
+
+            </div>
+            <!--end of col-->
+          </div>
+          <!--end of row-->
+
+          <div class="row justify-content-around align-items-center">
+            <div class="col-12 col-md-6 col-lg-5 order-md-2">
+              <ul class="nav" id="myTab" role="tablist">
+                <li>
+                  <a class="active card" id="tab-1" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">
+                    <div class="card-body">
+                      <h5>Mission</h5>
+                      <p align="justify">
+                        To Create innovative ways to bridge the financial inclusion gap,liberate Nigerians and people in other parts of the world through the provision of sound creative financing solutions that will help them and build the economy.
+                      </p>
+                    </div>
+                  </a>
+                </li>
+                <li>
+                  <a class="card" id="tab-2" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">
+                    <div class="card-body">
+                      <h5>Vision</h5>
+                      <p align="justify">
+                        Be the first one stop finance provision hub using innovation and technology  to constantly create unique finance solutions with delightful customer experience.
+                      </p>
+                    </div>
+                  </a>
+                </li>
+                <li>
+                  <a class="card" id="tab-3" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">
+                    <div class="card-body">
+                      <h5>About Us</h5>
+                      <p align="justify">
+                      Tactika is a financial solution provider for delivering tailor made offerings for our many customers using technology as an enabler and push them to helping achieve their goals be it personal family or business.
+                      </p>
+                    </div>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <!--end of col-->
+            <div class="col-12 col-md-6 order-md-1">
+              <div class="tab-content" id="myTabContent">
+                <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="tab-1">
+                  <img alt="Image" class="img-fluid shadow rounded-circle dotter" src="assets/img/be22.png" />
+                </div>
+                <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="tab-2">
+                  <img alt="Image" class="img-fluid shadow rounded-circle dotter" src="assets/img/be11.png" />
+                </div>
+                <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="tab-3">
+                  <img alt="Image" class="img-fluid shadow rounded-circle dotter" src="assets/img/be1.jpg" />
+                </div>
+              </div>
+            </div>
+            <!--end of col-->
+          </div>
+          <!--end of row-->
+        </div>
+        <!--end of container-->
+      </section>
+      <!--end of section-->
+      <section class="bg-gradient p-0 text-white">
+        <svg class="decorative-divider" preserveAspectRatio="none" viewbox=" 0 0 100 100">
+          <polygon fill="#F8F9FB" points="0 0 100 0 100 100"></polygon>
+        </svg>
+        <div class="container">
+          <div class="row justify-content-center section-intro">
+            <div class="col-auto">
+              <h2 class="h1" id="faq">Frequently Asked Questions</h2>
+            </div>
+            <!--end of col-->
+          </div>
+          <!--end of row-->
+          <ul class="row feature-list feature-list-sm justify-content-center">
+
+            <li class="col-12 col-md-6 col-lg-5">
+              <div class="card">
+                <div class="card-body">
+                  <h6>How does payment work?</h6>
+                  <p align="justify">
+                    Register as a new user and then on your dashboard there are range of options to choose for applying for loan, the process is easy and fast which ranges from BVN verification to updating Bio and loan amount requests.
+                  </p>
+                </div>
+              </div>
+            </li>
+
+            <li class="col-12 col-md-6 col-lg-5">
+              <div class="card">
+                <div class="card-body">
+                  <h6>How do i invest in Tactika?</h6>
+                  <p align="justify">
+                    Go to the Loans page and register and then proceed to fill your bio, then enter the investment amount and Tactika team would contact you, our rates are fixed and practices transparent with no hidden charges.
+                  </p>
+                </div>
+              </div>
+            </li>
+
+            <li class="col-12 col-md-6 col-lg-5">
+              <div class="card">
+                <div class="card-body">
+                  <h6>What do you mean by ‘unlimited’?</h6>
+                  <p align="justify">
+                    When we say unlimited we mean to say that we offer loans to anyone irrespective of portfolio whether you are a student or in the working class or even running a personal Business, we give loans.
+                  </p>
+                </div>
+              </div>
+            </li>
+
+            <li class="col-12 col-md-6 col-lg-5">
+              <div class="card">
+                <div class="card-body">
+                  <h6>What do you mean by personal Loans?</h6>
+                  <p align="justify">
+                    Personal Loans can be accessed by anyone and everyone for meeting basic needs to high budget demands,Tactika personal loan is designed to help you achieve your goals with ease and in short time frames.
+                  </p>
+                </div>
+              </div>
+            </li>
+
+            <li class="col-12 col-md-6 col-lg-5">
+              <div class="card">
+                <div class="card-body">
+                  <h6>Is this a subscription?</h6>
+                  <p align="justify">
+                    Tactika does not run subscription packages, rather you request for loans and pay back as when due,we furthermore have terms and conditions together with the policies guiding our terms of service in real time.
+                  </p>
+                </div>
+              </div>
+            </li>
+
+            <li class="col-12 col-md-6 col-lg-5">
+              <div class="card">
+                <div class="card-body">
+                  <h6>Am i restricted to an Amount?</h6>
+                  <p align="justify">
+                    Yes, the maximum amount Tactika offers to loan requests are from one thousand to a range of five million naira (5,000,000) we do not offer above that, but if in any case an amount above that is required kindly contact the Team
+                  </p>
+                </div>
+              </div>
+            </li>
+
+          </ul>
+          <!--end of row-->
+            <!--end of col-->
+          </div>
+          <!--end of row-->
+        </div>
+        <!--end of container-->
+      </section>
+      <!--end of section-->
+      <footer class="footer-short well">
+        <div class="container">
+          <hr>
+          <nav class="row justify-content-between align-items-center">
+            <div class="col-auto">
+              <ul class="list-inline">
+                <li class="list-inline-item">
+                  <a href="#">
+                    <img alt="Image" src="assets/img/logo.png" width="100px"/>
+                  </a>
+                </li>
+                <li class="list-inline-item">
+                  <a href="/privacy"><font color="gray">Privacy Policy</font></a>
+                </li>
+                <li class="list-inline-item">
+                  <a href="/terms"><font color="gray">Terms</font></a>
+                </li>
+              </ul>
+            </div>
+            <!--end of col-->
+            <div class="col-auto text-sm-right">
+              <ul class="list-inline">
+                <li class="list-inline-item">
+                  <a href="#"><i class="socicon-twitter"></i></a>
+                </li>
+                <li class="list-inline-item">
+                  <a href="#"><i class="socicon-facebook"></i></a>
+                </li>
+              </ul>
+            </div>
+            <!--end of col-->
+          </nav>
+          <!--end of row-->
+          <div class="row">
+            <div class="col">
+              <small>&copy; <?php echo date("Y");?><font color="gray"> Tactika All Rights Reserved crafted by MixedFraction</font></small>
+            </div>
+            <!--end of col-->
+          </div>
+          <!--end of row-->
+        </div>
+        <!--end of container-->
+      </footer>
+    </div>
+
+    <!-- Required vendor scripts (Do not remove) -->
+    <script type="text/javascript" src="{{assets(js/jquery.js)}}"></script>
+    <script type="text/javascript" src="{{assets(js/popper.min.js)}}"></script>
+    <script type="text/javascript" src="{{assets(js/bootstrap.js)}}"></script>
+
+    <!-- Optional Vendor Scripts (Remove the plugin script here and comment initializer script out of index.js if site does not use that feature) -->
+
+    <!-- AOS (Animate On Scroll - animates elements into view while scrolling down) -->
+    <script type="text/javascript" src="{{assets(js/aos.js)}}"></script>
+    <!-- Flatpickr (calendar/date/time picker UI) -->
+    <script type="text/javascript" src="{{assets(js/flatpickr.min.js)}}"></script>
+    <!-- Flickity (handles touch enabled carousels and sliders) -->
+    <script type="text/javascript" src="{{assets(js/flickity.pkgd.min.js)}}"></script>
+    <!-- jarallax (parallax effect and video backgrounds) -->
+    <script type="text/javascript" src="{{assets(js/jarallax.min.js)}}"></script>
+    <script type="text/javascript" src="{{assets(js/jarallax-video.min.js)}}"></script>
+    <script type="text/javascript" src="{{assets(js/jarallax-element.min.js)}}"></script>
+    <!-- ScrollMonitor (manages events for elements scrolling in and out of view) -->
+    <script type="text/javascript" src="{{assets(js/scrollMonitor.js)}}"></script>
+    <!-- jQuery smartWizard facilitates steppable wizard content -->
+    <script type="text/javascript" src="{{assets(js/jquery.smartWizard.js)}}"></script>
+    <!-- Smooth scroll (animation to links in-page)-->
+    <script type="text/javascript" src="{{assets(js/smooth-scroll.polyfills.min.js)}}"></script>
+    <!-- Prism (displays formatted code boxes) -->
+    <script type="text/javascript" src="{{assets(js/prism.js)}}"></script>
+    <script type="text/javascript" src="{{assets(js/zoom-vanilla.min.js)}}"></script>
+
+    <!-- Required theme scripts (Do not remove) -->
+    <script type="text/javascript" src="{{assets(js/theme.js)}}"></script>
+  </body>
+
+</html>
